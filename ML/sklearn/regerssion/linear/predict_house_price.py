cd73b281339362a07a585a61d2d455e84e4d236e
sunLei <2372388646@qq.com>
2018-12-01 19:29:59 +0800

    sunlei

--- ML/sklearn/regerssion/linear/predict_house_price.py
+++ ML/sklearn/regerssion/linear/predict_house_price.py
@@ -1,15 +1,16 @@
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt  # 可视化库类似matlab
-price=datasets.load_boston()
-price_x=price.data
-price_y=price.target
+
+price = datasets.load_boston()
+price_x = price.data
+price_y = price.target
 print(price_x)
 print(price_y)
-model=LinearRegression()
-model.fit(price_x,price_y)
-print(model.predict(price_x[:4,:]))
+model = LinearRegression()
+model.fit(price_x, price_y)
+print(model.predict(price_x[:4, :]))
 print(price_y[:4])
-x,y=datasets.make_regression(n_samples=100,n_features=1,n_targets=1,noise=10)
-plt.scatter(x,y)
+x, y = datasets.make_regression(n_samples=100, n_features=1, n_targets=1, noise=10)
+plt.scatter(x, y)
 plt.show()
