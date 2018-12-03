d27de0ddb15fb31493ec13972e9bf95088dd3f80
sunLei <2372388646@qq.com>
2018-12-03 19:42:30 +0800

    sunlei

---
+++ scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["readthedocs.io"]
+    # 为每一个url创建一个request
+    start_urls=[
+        "https://scrapy-chs.readthedocs.io/zh_CN/0.24/intro/tutorial.html",
+        "https://scrapy-chs.readthedocs.io/zh_CN/0.24/intro/tutorial.html"
+    ]
+    # request请求成功回调函数
+    def parse(self, response):
+        filename=response.url.split("/")[-2]
+        with open(filename,'wb') as f:
+            f.write(response.body)
