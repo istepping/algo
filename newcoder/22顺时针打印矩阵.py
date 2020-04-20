bd7f85ab1e9b80a224b5aa6f141ccaa93130aefd
sunLei <2372388646@qq.com>
2020-04-20 18:43:03 +0800

    newcoder

--- newcoder/22顺时针打印矩阵.py
+++ newcoder/22顺时针打印矩阵.py
@@ -5,12 +5,43 @@
 
 
 class Solution:
+    def __init__(self):
+        self.a = list()
+
+    def out_circle(self, matrix):
+        row, column = len(matrix), len(matrix[0])
+        if row == 1:
+            for i in range(column):
+                self.a.append(matrix[0][i])
+            return
+        elif column == 1:
+            for i in range(row):
+                self.a.append(matrix[i][0])
+            return
+        else:
+            for i in range(column - 1):
+                self.a.append(matrix[0][i])
+            for i in range(row):
+                self.a.append(matrix[i][column - 1])
+            for i in list(range(column - 2, -1, -1)):
+                self.a.append(matrix[row - 1][i])
+            for i in range(row - 2, 0, -1):
+                self.a.append(matrix[i][0])
+        if row > 2 and column>2:
+            matrix = matrix[1:row - 1]
+            new_matrix = list()
+            for i in matrix:
+                i.pop(0)
+                i.pop(-1)
+                new_matrix.append(i)
+            self.out_circle(new_matrix)
+
     # matrix类型为二维列表，需要返回列表
     def printMatrix(self, matrix):
-        d1, d2 = matrix.shape
-        a = list()
-        n = int(d1 / 2)
-        for i in range(n):
-            for j in range(d2):
-                a.append()
-            if n % 2 != 0 and i == n - 1:
+        self.out_circle(matrix)
+        return self.a
+
+
+s = Solution()
+a = s.printMatrix([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+print(a)
